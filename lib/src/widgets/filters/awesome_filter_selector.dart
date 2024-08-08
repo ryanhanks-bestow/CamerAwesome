b3c712e119c7d4b6e16482ec09755d60a8dddb58
ryanhanks-bestow <87093311+ryanhanks-bestow@users.noreply.github.com>
2024-08-08 14:57:06 -0500

    Hide CarouselController from material.dart

--- lib/src/widgets/filters/awesome_filter_selector.dart
+++ lib/src/widgets/filters/awesome_filter_selector.dart
@@ -1,6 +1,6 @@
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 import 'package:flutter/services.dart';
 
 class AwesomeFilterSelector extends StatefulWidget {
